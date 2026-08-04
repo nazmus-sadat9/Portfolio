9ee92f084b59ec2fa2125a48827eabb68fb4e07a
Nazmus Sadat <prosadat505@gmail.com>
2026-08-04 10:38:44 +0600

    fix: frontend url

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -34,7 +34,7 @@ exports.sendEmail = async (req, res)=>{
       from: process.env.AUTHOR_GMAIL,
       to: email,
       subject: "WELCOME",
-      html: `<div style="font-family: sans-serif; padding: 1rem; background-color: #121212; color: #ffffff;"><h2>Welcome ${name},</h2><br> <p style="line-height: 1.6; font-weight: 300;">Thank you so much for explore my portfolio. If you have any openion about my website you can tell me.</p> <br><br><br> <p style="text-align: center; border-top: 1px solid #ffffff; padding: 1rem 0; font-weight: 200;">&copy; 2026 SADAT. All rights reserved</p></div>`
+      html: `<div style="font-family: sans-serif; padding: 1rem; background-color: #121212; color: #ffffff;"><h2>Welcome ${name},</h2><br> <p style="line-height: 1.6; font-weight: 300;">Thank you so much for explore my portfolio. If you have any opinion about my website you can tell me.</p> <br><br><br> <p style="text-align: center; border-top: 1px solid #ffffff; padding: 1rem 0; font-weight: 200;">&copy; 2026 SADAT. All rights reserved</p></div>`
     };
 
     await emailEngine.sendMail(usermail);
