d876a8abbfde13d03d930703778b73818d9f7a3a
Nazmus Sadat <prosadat505@gmail.com>
2026-08-09 00:42:22 +0600

    testing api

--- frontend/app/Home/page.js
+++ frontend/app/Home/page.js
@@ -1,10 +1,13 @@
 import HomeGrid from "../Components/Home/HomeGrid.jsx";
 
+import Temp from "../Components/Temp.jsx";
+
 export default function Page() {
 
   return(
     <div className="pt-[12%] h-screen w-screen bg-[#ffffdb] flex justify-center items-center">
-    <HomeGrid />
+      <HomeGrid />
+      <Temp />
     </div>
   );
 }
